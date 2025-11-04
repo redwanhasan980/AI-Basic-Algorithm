679f6feb37cece3bf880e5fd7d2e28702d67b34b
Redwan <redwanhasan980@gmail.com>
2025-11-04 12:16:57 +0600

    Basic Hill Climbing 8 Puzzle

---
+++ InformedSearch/LocalSearch/HillClimbing.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+using namespace std;
+int calculateHeuristic( vector<vector<int>>& state, vector<vector<int>>& goal){
+int heuristic = 0;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if(state[i][j] != 0){
+                for(int x = 0; x < 3; x++){
+                    for(int y = 0; y < 3; y++){
+                        if(state[i][j] == goal[x][y]){
+                            heuristic += abs(i - x) + abs(j - y);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return heuristic;
+}
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    vector<vector<int>> State(3,vector<int>(3));
+    vector<vector<int>> GoalState(3,vector<int>(3));
+    int i,j;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            cin>>State[i][j];
+        }
+    }
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            cin>>GoalState[i][j];
+        }
+    }
+    int it=1;
+    while(true){
+        int h=calculateHeuristic(State,GoalState);
+        cout<<"-----------------------"<<endl;
+        cout<<"Iteration: "<<it++<<endl;
+        cout<<"Current Heuristic: "<<h<<endl;
+        cout<<"Current State: "<<endl;
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                cout<<State[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        if(h==0){
+            cout<<"Goal Found"<<endl;
+            break;
+        }
+        vector<vector<int>>neibours[5];
+        
+        int blankX,blankY;
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                if(State[i][j]==0){
+                    blankX=i;
+                    blankY=j;
+                }
+            }
+        }
+        // Blank Space move up if possible
+        if(blankX-1>=0){
+            vector<vector<int>> newState=State;
+            swap(newState[blankX][blankY],newState[blankX-1][blankY]);
+            neibours[0]=newState;
+        }
+        else {
+            neibours[0]=State;
+        }
+        // Blank Space move down if possible
+        if(blankX+1<3){
+            vector<vector<int>> newState=State;
+            swap(newState[blankX][blankY],newState[blankX+1][blankY]);
+            neibours[1]=newState;
+        }
+        else {
+            neibours[1]=State;
+        }
+        // Blank Space move left if possible
+        if(blankY-1>=0){
+            vector<vector<int>> newState=State;
+            swap(newState[blankX][blankY],newState[blankX][blankY-1]);
+            neibours[2]=newState;
+        }
+        else 
+         neibours[2]=State;
+        // Blank Space move right if possible
+        if(blankY+1<3){
+            vector<vector<int>> newState=State;
+            swap(newState[blankX][blankY],newState[blankX][blankY+1]);
+            neibours[3]=newState;
+        }
+        else 
+         neibours[3]=State;
+
+        int minHeuristic=INT_MAX;
+        int index=-1;
+        for(i=0;i<4;i++){
+            int h1=calculateHeuristic(neibours[i],GoalState);
+            if(h1<minHeuristic){
+                minHeuristic=h1;
+                index=i;
+            }
+        }
+        if(minHeuristic>=h){
+            cout<<"No better neighbour. Local Minima reached."<<endl;
+            break;
+        }
+        else 
+          State=neibours[index];
+
+    }
+
+}
